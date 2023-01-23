ae1fd06ead36e13b32d67ee590518447885de6a1
Marco <posta.email3001@gmail.com>
2023-01-23 23:20:06 +0100

    effettuate interrogazioni del db con JOIN

--- query2.sql
+++ query2.sql
@@ -20,16 +20,38 @@ GROUP BY department_id;
 
 -- #### Join
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
-
+SELECT students.*
+FROM students 
+	JOIN degrees
+    	ON students.degree_id = degrees.id
+WHERE degrees.name LIKE 'Corso di Laurea in Economia';
 
 -- 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
-
+SELECT degrees.*
+FROM degrees
+	JOIN departments
+    	ON degrees.department_id = departments.id
+WHERE departments.name LIKE 'Dipartimento di Neuroscienze'
+	AND degrees.level LIKE 'magistrale';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
-
+SELECT courses.*
+FROM courses
+	JOIN course_teacher
+    	ON courses.id = course_teacher.course_id
+    JOIN teachers
+    	ON teachers.id = course_teacher.teacher_id
+WHERE teachers.name LIKE 'Fulvio'
+	AND teachers.surname LIKE 'Amato';
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
-
+SELECT *
+FROM students
+    JOIN degrees
+        ON degrees.id = students.degree_id
+    JOIN departments
+        ON departments.id = degrees.department_id  
+ORDER BY students.surname, students.name;
 
 -- ##### BONUS
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
